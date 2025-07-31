e48164489bf9f166da30dba74a3394bf0c0c22ad
pckielbasa <pckielbasa@gmail.com>
2025-07-31 19:16:01 +0200

    Initial commit from local clean project

---
+++ src/main/java/com/pkielbasa/CookBook/CookBookApplication.java
@@ -0,0 +1,13 @@
+package com.pkielbasa.CookBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkielbasa/CookBook/CookBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkielbasa.CookBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
